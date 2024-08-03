db79a95c91f65179a11cf498370f8b7d4cad3b2d
g-xianhui <g_xianhui@163.com>
2024-08-03 08:04:55 +0800

    add login ui and NetworkManager

---
+++ client/Assets/Scripts/LoginUI.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginUI : MonoBehaviour
+{
+    public Text ServerAddressText;
+    public Text AccountText;
+
+    public void OnLoginResult(bool isSuccess)
+    {
+        if (isSuccess)
+        {
+            Debug.Log("on login success");
+            SceneManager.LoadScene("DemoScene", LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.Log("on login failed");
+        }
+    }
+
+    public void OnLoginButtonClick()
+    {
+        string serverAddress = ServerAddressText.text;
+        string account = AccountText.text;
+
+        if (serverAddress == string.Empty || account == string.Empty)
+        {
+            Debug.Log("offline test");
+            SceneManager.LoadScene("DemoScene", LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.Log($"online test, server: {serverAddress}, account: {account}");
+            string[] arr = serverAddress.Split(':');
+            int port;
+            if (arr.Length != 2 || !Int32.TryParse(arr[1], out port))
+            {
+                Debug.Log($"invalid address {serverAddress}");
+                return;
+            }
+            string host = arr[0];
+
+            NetworkManager.Instance.Connect(host, port, OnLoginResult);
+        }
+    }
+}
---
+++ client/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager Instance;
+
+    private TcpClient _client;
+    private NetworkStream _stream;
+
+    public bool DoPrint = true;
+    public bool IsConnected = false;
+
+    private int _bytesWrite = 0;
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public async Task<bool> ConnectAsync(string host, int port)
+    {
+        _client = new TcpClient();
+        try
+        {
+            Task connectTask = _client.ConnectAsync(host, port);
+            Task delayTask = Task.Delay(2000);
+            if (await Task.WhenAny(connectTask, delayTask) == delayTask)
+            {
+                Debug.Log("connect timeout");
+                return false;
+            }
+        }
+        catch (SocketException  e)
+        {
+            Debug.Log(e.ToString());
+            return false;
+        }
+        catch (ObjectDisposedException e)
+        {
+            Debug.Log(e.ToString());
+            return false;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            return false;
+        }
+
+        Debug.Log("connect to server success");
+        return true;
+    }
+
+    public async void Connect(string host, int port, Action<bool> connectCallback)
+    {
+        Debug.Log("connecting to server");
+        IsConnected = await ConnectAsync(host, port);
+        connectCallback(IsConnected);
+    }
+}
